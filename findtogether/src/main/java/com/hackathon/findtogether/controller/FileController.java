6ec7de97210fd082cf00ea9fe3d2ea6eb9462831
김송이 <ksl2950@naver.com>
2022-06-23 04:41:43 +0900

    feat : 게시물 조회 필터링
    - 찾아주세요, 찾았어요 조회

--- findtogether/src/main/java/com/hackathon/findtogether/controller/FileController.java
+++ findtogether/src/main/java/com/hackathon/findtogether/controller/FileController.java
@@ -4,18 +4,10 @@ import com.hackathon.findtogether.service.FileService;
 import com.hackathon.findtogether.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 @Slf4j
 @RestController
@@ -25,41 +17,13 @@ public class FileController {
     private final FileService fileService;
     private final PostService postService;
 
-    @PostMapping(value = "/upload")
+    @PostMapping(value = "/api/v1/file")
     public Response uploadFile(MultipartFile file) {
         if(file.isEmpty()) {
             /* 파일을 업로드 하지 않았을 경우 처리 */
         }
-
         String fileName = fileService.saveFile(file);
-//        String downloadURI = ServletUriComponentsBuilder.fromCurrentContextPath()
-//                .path("/download/")
-//                .path(file.getName())
-//                .toUriString();
 
         return new Response(200,true,"created image successfully", fileName);
     }
-
-
-//    @GetMapping(value = "/download")
-//    public ResponseEntity<?> downloadFile(String fileName) throws FileNotFoundException {
-//
-//        Resource resource = fileService.loadFile(fileName);
-//
-//        String contentType = null;
-//        try {
-//            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
-//        } catch (IOException ex) {
-//            log.info("Could not determine file type.");
-//        }
-//
-//        // Fallback to the default content type if type could not be determined
-//        if(contentType == null) {
-//            contentType = "application/octet-stream";
-//        }
-//        return ResponseEntity.ok()
-//                .contentType(MediaType.parseMediaType(contentType))
-//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-//                .body(resource);
-//    }
 }
--- findtogether/src/main/java/com/hackathon/findtogether/repository/PostSearchRepository.java
+++ findtogether/src/main/java/com/hackathon/findtogether/repository/PostSearchRepository.java
@@ -1,4 +1,69 @@
 package com.hackathon.findtogether.repository;
 
+import com.hackathon.findtogether.domain.Post;
+import com.hackathon.findtogether.domain.PostType;
+import com.hackathon.findtogether.domain.QPost;
+
+import com.hackathon.findtogether.domain.ResolvingStatus;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+
+@Repository
 public class PostSearchRepository {
+
+    private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
+
+    public PostSearchRepository(EntityManager em) {
+        this.em = em;
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    // 찾아주세요
+    public List<Post> findPostByLost(){
+        return em.createQuery("select m " +
+                "from Post m " +
+                "where m.postType =:postType", Post.class)
+                .setParameter("postType", PostType.LOST)
+                .getResultList();
+    }
+
+    // 찾았어요
+    public List<Post> findPostByDiscovery(){
+        return em.createQuery("select m " +
+                "from Post m " +
+                "where m.postType =:postType", Post.class)
+                .setParameter("postType", PostType.DISCOVERY)
+                .getResultList();
+    }
+
+    // 최신순
+    public List<Post> findPostByLatest(){
+        QPost p = new QPost("p");
+
+        List<Post> posts = queryFactory
+                .selectFrom(p)
+                .orderBy(p.createdAt.desc())
+                .fetch();
+        return posts;
+    }
+
+    // 해결
+    public List<Post> findPostByWaiting(){
+        QPost p = new QPost("p");
+
+        List<Post> posts = queryFactory
+                .selectFrom(p)
+                .orderBy(p.createdAt.desc())
+                .fetch();
+        return posts;
+    }
+    // 미해결
+
 }
